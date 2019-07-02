5195ffe1dd2ce7decaf60c562e88420689b18725
Marco Porcho <maporcho@gmail.com>
2019-07-02 19:33:25 -0300

    starting to code StoryBrain

--- lib/story.dart
+++ lib/story.dart
@@ -1,14 +1,20 @@
 class Story {
   String _storyTitle;
   String _choice1;
-  String _choiceTwo;
+  String _choice2;
 
-  Story(this._storyTitle, this._choice1, this._choiceTwo);
+  //Story(this._storyTitle, this._choice1, this._choiceTwo);
 
-  String get choiceTwo => _choiceTwo;
+  Story({String storyTitle, String choice1, String choice2}) {
+    _storyTitle = storyTitle;
+    _choice1 = choice1;
+    _choice2 = choice2;
+  }
 
-  set choiceTwo(String value) {
-    _choiceTwo = value;
+  String get choice2 => _choice2;
+
+  set choice2(String value) {
+    _choice2 = value;
   }
 
   String get choice1 => _choice1;
